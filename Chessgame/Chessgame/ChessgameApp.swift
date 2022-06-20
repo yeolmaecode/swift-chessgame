0f0230d6433e4dae46d78e4f5dcb04f8aa74fb3f
sam.1 <sam.1@kakaocorp.com>
2022-06-20 19:21:55 +0900

    체스게임 프로젝트 생성

---
+++ Chessgame/Chessgame/ChessgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessgameApp.swift
+//  Chessgame
+//
+//  Created by yeolmaecode on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct ChessgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
